36cdabda0428f37a743ccdfe72dd15a6abefc5fc
j-gayathri <gayathrijayagopal96@gmail.com>
2020-12-25 12:48:53 +0530

    Code refactor

--- sketch.js
+++ sketch.js
@@ -14,7 +14,6 @@ function setup() {
 	createCanvas(1600, 700);
 	rectMode(CENTER);
 
-
 	engine = Engine.create();
 	world = engine.world;
 
@@ -29,10 +28,6 @@ function setup() {
 	bobObject3=new bob(startBobPositionX,startBobPositionY,bobDiameter);
 	bobObject4=new bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
 	bobObject5=new bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
-	
-	
-	//Create a Ground
-	
 
 	var render = Render.create({
 	  element: document.body,
@@ -52,51 +47,7 @@ function setup() {
 	rope4=new rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
 	rope5=new rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
 
-	/*constraint1={
-		bodyA:bobObject1.body,
-		bodyB:roofObject.body,
-		pointB: {x:-bobDiameter*2, y:0}
-	}
 
-	constraint2={
-		bodyA:bobObject2.body,
-		bodyB:roofObject.body,		
-		pointB: {x:-bobDiameter, y:0}
-	}
-
-
-	constraint3={
-		bodyA:bobObject3.body,
-		bodyB:roofObject.body,		
-		pointB: {x:0, y:0}
-
-	}
-
-	constraint4={
-		bodyA:bobObject4.body,
-		bodyB:roofObject.body,		
-		pointB: {x:bobDiameter, y:0}	
-
-	}
-
-	constraint5={
-		bodyA:bobObject5.body,
-		bodyB:roofObject.body,		
-		pointB: {x:bobDiameter*2, y:0}
-	}
-
-	var pendulum1=Constraint.create(constraint1)
-	var pendulum2=Constraint.create(constraint2)
-	var pendulum3=Constraint.create(constraint3)
-	var pendulum4=Constraint.create(constraint4)
-	var pendulum5=Constraint.create(constraint5)
-
-	World.add(world, pendulum1);
-	World.add(world, pendulum2);
-	World.add(world, pendulum3);
-	World.add(world, pendulum4);
-	World.add(world, pendulum5);
-	*/
 	Engine.run(engine);
 	//Render.run(render);
   
@@ -118,14 +69,6 @@ function draw() {
   bobObject3.display();
   bobObject4.display();
   bobObject5.display();
- 
-  
-  
-	
-  
- 
-  
-  
  
 }
 
